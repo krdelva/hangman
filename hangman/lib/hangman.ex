9fd3abda25b8be57a7a95d18057918332cf88f33
krdelva <kevin.r.delva@gmail.com>
2022-10-05 10:58:51 -0400

    Add implementation of game in separate folder, access implmentation in API

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,18 +1,23 @@
 defmodule Hangman do
-  @moduledoc """
-  Documentation for `Hangman`.
-  """
 
-  @doc """
-  Hello world.
+  alias Hangman.Impl.Game  # , as: Game
 
-  ## Examples
+  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type game :: any
+  @type tally :: %{
+    turns_left: integer,
+    game_state: state,
+    letters: list(String.t),
+    used: list(Stirng.t),
+  }
 
-      iex> Hangman.hello()
-      :world
 
-  """
-  def hello do
-    :world
+  @spec new_game :: game
+  defdelegate new_game, to: Game
+
+  @spec make_move(game, String.t) :: { game, tally }
+  def make_move(_game, _guess) do
+
   end
+
 end
